2cf0058b5179b297d11f1fa3637c596ff05973ef
Hasam Hasam <h.hasam@outlook.com>
2023-04-30 20:24:15 +1000

    Restructure

--- hasam/src/components/functions/Arrow.js
+++
@@ -1,9 +0,0 @@
-/**
- * Arrow Function
- * Shorthand way of defining function in JS. 
- * Usage:
- * Useful for defining functions as callbacks 
- * Useful for array methods
- */
-
-const add = (a,b) => a + b; 
