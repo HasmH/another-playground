be3853560c6633dc037a47eb8b7e09828916719f
HasmH <h.hasam@outlook.com>
2023-05-01 23:17:09 +1000

    Tried doing typed animation

--- hasam/src/App.js
+++ hasam/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import StylePlayground from './components/StylePlayground/StylePlayground';
+import Intro from './components/Intro/Intro';
 function App() {
   return (
     <div className="App">
-      <StylePlayground />
+      <Intro />
     </div>
   );
 }
