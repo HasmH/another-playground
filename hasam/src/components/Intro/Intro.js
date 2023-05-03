10df3cf32427ba82218d404ba3bad5c9e528535b
Hasam Hasam <h.hasam@outlook.com>
2023-05-03 22:37:34 +1000

    Attempt at wrapping image in Paper mui component

--- hasam/src/components/Intro/Intro.js
+++ hasam/src/components/Intro/Intro.js
@@ -1,37 +1,42 @@
 import './Intro.style.css';
 import { Typewriter } from 'react-simple-typewriter';
-
-/**
- * Component that takes in a name and tag line, and animates in typing way
- */
+import headshot from './reactHeadshot.jpg';
+import { Grid, Paper } from '@mui/material';
 
 function Intro() {
     return(
-        <div>
-            <div className='intro-heading'>
+        <Grid container spacing={4}>
+            <Grid item xs={12} md={6}>
                 <Typewriter 
-                    words={['Hasam.']}
-                    loop
-                    cursor
-                    cursorStyle=' '
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={30000}
-                />
-            </div>
-            <div>
-                <div className="intro-subheading">
+                        words={['Hasam.']}
+                        loop
+                        cursor
+                        cursorStyle=' '
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={30000}
+                    />
+                
                     <Typewriter 
-                        words={['Hello!', ' नमस्ते', 'Bonjour!', 'Hola!', 'Ciao!', 'مرحبا', 'Slaw!', 'こんにちは', 'Привет' ]}
+                        words={['Hello!', 'नमस्ते', 'Bonjour!', 'Hola!', 'Ciao!', 'مرحبا', 'Slaw!', 'こんにちは', 'Привет' ]}
                         loop
                         cursor
                         cursorStyle='|'
                         typeSpeed={70}
                         deleteSpeed={50}
                     />
+            </Grid>
+            <Grid item xs={12} md={6}>
+                <div className='head-shot-paper-div'>
+                    <Paper elevation={3} className='head-shot'>
+                        <img
+                        src={headshot}
+                        alt="me"
+                        />
+                    </Paper>
                 </div>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     );
 }
 
