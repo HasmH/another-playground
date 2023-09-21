1b5f017c491fefdaec04541ebcfd31fd12b5df58
Hasam Hasam <h.hasam@outlook.com>
2023-09-21 20:03:44 +1000

    Restarting

--- hasam/learning/functions/Constructor.js
+++
@@ -1,8 +0,0 @@
-/**
- * Function Constructor 
- * defining function using Function connstructor class 
- * Pretty useless?
- */
-
-const add = new Function("a", "b", "return a + b");
-
--- hasam/learning/functions/Decleration.js
+++
@@ -1,8 +0,0 @@
-/**
- * Function Decleration
- * Named function taking in 0 or more paramaters 
- */
-
-function add(a, b) {
-    return a + b;
-}
